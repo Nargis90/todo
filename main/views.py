881d276e1dfe144b56ca6d4d040b24e7c4b2eb6e
Nargis90 <temirkulova.n@gmail.com>
2021-01-24 01:09:23 +0600

    added test page

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from .models import Todo
 
 # Create your views here.
 def homepage(request):
     return render(request,"index.html")
 
 def test(request):
-    return render(request,"test.html")   
+
+    todo_list = Todo.objects.all()
+    return render(request,"test.html", {"todo_list":todo_list})   
 
 def second(request):
     return HttpResponse("test 2 page")
