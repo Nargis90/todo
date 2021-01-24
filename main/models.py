ddb0242de00583b60a86000616a7b4f7f96e178d
Nargis90 <temirkulova.n@gmail.com>
2021-01-24 15:59:38 +0600

    create book model

--- main/models.py
+++ main/models.py
@@ -6,5 +6,15 @@ class Todo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+
+class Book(models.Model):
+    title=models.CharField(max_length=50)
+    subtitle=models.CharField(max_length=50)
+    description=models.TextField()
+    price=models.IntegerField()
+    genre=models.CharField(max_length=50)
+    author=models.CharField(max_length=100)
+    year=models.DateField()
     
     # Create your models here.
